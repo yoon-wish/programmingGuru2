415f23007e57c7d8fe390ba3562c512e48a66459
Sowon Yun <sowon5685@naver.com>
2021-01-25 14:32:20 +0900

    Guru2_Android

---
+++ app4_TiltSensor/app/src/main/java/com/example/myapplication/TiltView.kt
@@ -0,0 +1,43 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.hardware.SensorEvent
+import android.view.View
+
+class TiltView(context: Context?) : View(context) {
+    private val greenPaint: Paint = Paint()
+    private val blackPaint: Paint = Paint()
+
+    private var cX: Float = 0f
+    private var cY: Float = 0f
+    private var xCoord: Float = 0f
+    private var yCoord: Float = 0f
+
+    init {
+        greenPaint.color = Color.GREEN
+        blackPaint.style = Paint.Style.STROKE
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) { //폰 화면의 중심심
+       cX = w/2f
+        cY = h/2f
+    }
+
+    override fun onDraw(canvas: Canvas?){
+        canvas?.drawCircle(cX, cY,100f,blackPaint) //중점 0,0 반지름 100
+        canvas?.drawCircle(xCoord+cX, yCoord+cY,100f,greenPaint)
+
+        canvas?.drawLine(cX-20, cY, cX+20,cY, blackPaint)
+        canvas?.drawLine(cX, cY-20, cX,cY+20, blackPaint)
+    }
+
+    fun onSensorEvent(event: SensorEvent) {
+        yCoord = event.values[0] * 20
+        xCoord = event.values[1] * 20
+
+        invalidate()
+    }
+}
